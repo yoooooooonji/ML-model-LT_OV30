7be637508f941e205130be8d1be5788da4ce4a13
Yoonji <yoonji.noh90@gmail.com>
2023-09-08 14:36:09 +0900

    roc

--- outcome_extract.R
+++ outcome_extract.R
@@ -8,26 +8,48 @@ ipak <-function(pkg){
   sapply(pkg,require,character.only=TRUE)
 }
 
-pkg <- c("readr", "MatchIt", "dplyr", "tidytext", "tidyverse", "lubridate", "reshape2", "psych", "gtsummary", "readxl", "MASS", "pROC") # nolint
+pkg <- c("readr", "MatchIt", "dplyr", "tidytext", "tidyverse", "lubridate", "reshape2", "psych", "gtsummary", "readxl", "MASS", "pROC", "Epi") # nolint
 ipak(pkg)
 ##########################################################################################################################################################
+# 함수
+optimal_lr.eta=function(x){
+  no=which.max(x$res$sens+x$res$spec)[1]
+  result=x$res$lr.eta[no]
+  result
+}
 
+optimal_cutpoint=function(x){
+   y=optimal_lr.eta(x)
+   b0=unname(x$lr$coeff[1])
+   b1=unname(x$lr$coeff[2])
+   result=(-log(1/y-1)-b0)/b1
+   result
+} 
+##########################################################################################################################################################
 # 1. data load
-setwd("/Users/yj.noh/Documents/GitHub/prj-ML-model-LT_OV30/")
 #data <- read.csv("raw_data_time.csv", fileEncoding = "cp949")
-data <- read_excel("raw_data_time.xlsx")
+data <- read_excel("prj-ML-model-LT_OV30/raw_data_time.xlsx")
 head(data)
 colSums(is.na(data))
 
 # 2. 데이터 정제
 data <- data %>% mutate(chk = ifelse((추천노출시간_Ai + 추천노출시간_일반 + 추천미발생시간유 + 추천미발생시간무 + 배차후취소시간) < 전체배차시간, 1,0))
-table(data$chk) # 90616, 25352 
+table(data$chk) # 90,616, 25,352 
 
 data_filter <- data  %>% filter(chk == 0)
-dim(data_filter) #90616 
+dim(data_filter) #90,616 
 
-# 3. 전체배차시간 - 고안시 초과 상관관계 
-cor(data_filter$전체배차시간, data_filter$notiOver) #0.46
+# 2-1 
+data_filter <- data_filter  %>% mutate(output_10 = ifelse(notiOver_min_max >= 10,1,0),
+                         output_20 = ifelse(notiOver_min_max >= 20, 1,0),
+                         output_5 = ifelse(notiOver_min_max >=5,1,0))
+
+table(data_filter$output_5) #5991
+table(data_filter$output_10) # 2720
+table(data_filter$output_20) #600
+
+# 3. 전체배차시간 - 고안시 roc
+# 함수
 
 png("boxplot_전처리전.png")
 boxplot(전체배차시간 ~ notiOver, data = data_filter, col = "orange")
@@ -48,49 +70,21 @@ max_1 <- as.numeric(max[2])
 max_0 
 max_1
 
-# 4. remove outlier
-data_rm <- data_filter %>% filter((notiOver == 0 & 전체배차시간 <= max_0) | (notiOver == 1 & 전체배차시간 <= max_1))
-dim(data_rm) # 81993 
+# 3-1. ouput_5 
+f_plot <- boxplot(전체배차시간 ~ output_5, data = data_filter, col = "orange")
+max <- f_plot$stats[5,]
+max_0 <- as.numeric(max[1])
+max_1 <- as.numeric(max[2])
+max_0 #7.03
+max_1 # 46.98
 
-test <- data_rm  %>% filter(notiOver == 0)
-summary(test$전체배차시간)
+data_rm <- data_filter %>% filter((output_5 == 0 & 전체배차시간 <= max_0) | (output_5 == 1 & 전체배차시간 <= max_1))
+dim(data_rm) # 81350
 
-test2 <- data_rm  %>% filter(notiOver == 1)
-summary(test2$전체배차시간)
+roc_1<- ROC(form = output_5 ~ 전체배차시간, data = data_rm, plot = "ROC")
+roc_2<- roc(data_rm$output_5, data_rm$전체배차시간)
 
-png("boxplot_전처리후.png")
-boxplot(전체배차시간 ~ notiOver, data = data_rm, col = "orange")
-dev.off()
-
-boxplot_stats <- data.frame(
-  Min = a_plot$stats[1, ],
-  Q1 = a_plot$stats[2, ],
-  Median = a_plot$stats[3, ],
-  Q3 = a_plot$stats[4, ],
-  Max = a_plot$stats[5, ]
-)
-print(boxplot_stats)
-
-
-# 5. roc 
-table(data_rm$notiOver) #0 : 67480, 1: 14513 
-
-roc_1 <- ROC(form = notiOver ~ 전체배차시간, data = data_rm, plot = "ROC")
-roc_2<- roc(data_rm$notiOver, data_rm$전체배차시간)
-
-roc_1
-roc_2 
-
-tiff("graph/ROC_Curve1.png", width = 1200, height = 1200)   
-plot.roc(roc_1,   
-         col="red",   
-         print.auc = TRUE,   
-         max.auc.polygon = TRUE,   
-         print.thres = TRUE, print.thres.pch=19, print.thres.col = "red",   
-         auc.polygon = TRUE, auc.polygon.col="#D1F2EB")   
-dev.off()  
-
-tiff("graph/ROC_Curve2.png", width = 1200, height = 1200)   
+tiff("prj-ML-model-LT_OV30/graph/ROC_curve_ov5.png", width = 1200, height = 1200)   
 plot.roc(roc_2,   
          col="red",   
          print.auc = TRUE,   
@@ -99,20 +93,73 @@ plot.roc(roc_2,
          auc.polygon = TRUE, auc.polygon.col="#D1F2EB")   
 dev.off()  
 
-optimal_lr.eta=function(x){
-  no=which.max(x$res$sens+x$res$spec)[1]
-  result=x$res$lr.eta[no]
-  result
-}
-optimal_lr.eta(roc_1)
 
-optimal_cutpoint=function(x){
-   y=optimal_lr.eta(x)
-   b0=unname(x$lr$coeff[1])
-   b1=unname(x$lr$coeff[2])
-   result=(-log(1/y-1)-b0)/b1
-   result
-} 
-optimal_cutpoint(roc_1) #4.316667
+optimal_lr.eta(roc_1)
+optimal_cutpoint(roc_1) # 6.51
+
+
+# output_10으로 테스트
+t_plot <- boxplot(전체배차시간 ~ output_10, data = data_filter, col = "orange")
+max <- t_plot$stats[5,]
+max_0 <- as.numeric(max[1])
+max_1 <- as.numeric(max[2])
+max_0 #7.7
+max_1 # 51.7
+
+data_rm <- data_filter %>% filter((output_10 == 0 & 전체배차시간 <= max_0) | (output_10== 1 & 전체배차시간 <= max_1))
+dim(data_rm) # 80779
+
+roc_1<- ROC(form = output_10 ~ 전체배차시간, data = data_rm, plot = "ROC")
+roc_2<- roc(data_rm$output_10, data_rm$전체배차시간)
+
+tiff("prj-ML-model-LT_OV30/graph/ROC_curve_ov10.png", width = 1200, height = 1200)   
+plot.roc(roc_2,   
+         col="red",   
+         print.auc = TRUE,   
+         max.auc.polygon = TRUE,   
+         print.thres = TRUE, print.thres.pch=19, print.thres.col = "red",   
+         auc.polygon = TRUE, auc.polygon.col="#D1F2EB")   
+dev.off()  
+
+
+optimal_lr.eta(roc_1)
+optimal_cutpoint(roc_1) # 7.66
+
+# output_20으로 테스트
+t_plot <- boxplot(전체배차시간 ~ output_20, data = data_filter, col = "orange")
+max <- t_plot$stats[5,]
+max_0 <- as.numeric(max[1])
+max_1 <- as.numeric(max[2])
+max_0 # 8.28
+max_1 # 61.53
+
+data_rm <- data_filter %>% filter((output_20 == 0 & 전체배차시간 <= max_0) | (output_20== 1 & 전체배차시간 <= max_1))
+dim(data_rm) # 80026
+
+roc_1<- ROC(form = output_20 ~ 전체배차시간, data = data_rm, plot = "ROC")
+roc_2<- roc(data_rm$output_20, data_rm$전체배차시간)
+
+tiff("prj-ML-model-LT_OV30/graph/ROC_curve_ov20.png", width = 1200, height = 1200)   
+plot.roc(roc_2,   
+         col="red",   
+         print.auc = TRUE,   
+         max.auc.polygon = TRUE,   
+         print.thres = TRUE, print.thres.pch=19, print.thres.col = "red",   
+         auc.polygon = TRUE, auc.polygon.col="#D1F2EB")   
+dev.off()  
+
+optimal_lr.eta(roc_1)
+optimal_cutpoint(roc_1) # 8.21
+
+
+
+
+
+
+
+
+
+
+
 
 
