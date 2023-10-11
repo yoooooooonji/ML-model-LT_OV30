d8980fa07c662ea876e210e43c4638dd4c31f68a
Yoonji <yoonji.noh90@gmail.com>
2023-10-11 22:43:42 +0900

    1011

---
+++ on_boarding.R
@@ -0,0 +1,46 @@
+# 0. install packages 
+options(scipen=10)
+
+ipak <-function(pkg){
+  new.pkg<-pkg[!(pkg %in% installed.packages()[,"Package"])]
+  if(length(new.pkg))
+    install.packages(new.pkg,dependencies=TRUE)
+  sapply(pkg,require,character.only=TRUE)
+}
+
+pkg <- c("readr", "MatchIt", "dplyr", "tidytext", "tidyverse", "lubridate", "reshape2", "psych", "gtsummary", "readxl", "MASS", "pROC", "Epi") # nolint
+ipak(pkg)
+##########################################################################################################################################################
+
+data <- read.csv('prj-ML-model-LT_OV30/on_boarding_data_7day.csv', encoding = 'cp949')
+head(data)
+str(data)
+dim(data)
+
+colSums(is.na(data))
+
+df <- subset(data, select = -c(brms_rider_id, first_available_date, business_start_day, recommender_rider_account_id, rider_no, last_working_day, 조회기간))
+
+df <- df  %>% filter(주요수행method == 'BIKE' & is_recom == 0)
+
+vars_to_convert <- setdiff(names(df), c('수행건수','실제수행기간', '기본배달비_중간값', '총배달비_중간값', 'not_working', '첫운행이후'))
+
+df[vars_to_convert] <- lapply(df[vars_to_convert], as.factor)
+str(df)
+
+df %>% 
+# tbl_strata(
+#     strata = outcome,~.x  %>% 
+    tbl_summary(
+        by = outcome,
+        type = all_continuous() ~ "continuous2",
+              statistic = all_continuous() ~ c("{mean} ({sd})", "{min}, {max}"),
+              missing_text = "(Missing value)", 
+              digits = list(all_continuous() ~ c(0, 1), 
+                            all_categorical() ~ c(0, 1))) %>%
+  add_overall() %>%
+  add_p(pvalue_fun = ~style_pvalue(., digits = 3)) %>%
+  bold_labels()
+
+
+
